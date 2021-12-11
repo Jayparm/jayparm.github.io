a6317b9a8b4a94e1dce5e0660873bc395e510bfa
Evan <coolgamer7point9@gmail.com>
2021-12-11 16:20:25 +0000

    Added jump game and ball game

---
+++ script/canvas-game.js
@@ -0,0 +1,6 @@
+const canvas = document.querySelector("canvas");
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+console.log(canvas);
+
+// Do this later: https://youtu.be/eI9idPTT0c4?t=244
